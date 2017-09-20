ab1ac0e1ef887cf73e8fc405bde28b4467d190ad
Robin Choud <robin.m.chou@gmail.com>
2017-09-20 14:22:28 +0800

    Add volume example

--- src/pcm/pcm.cpp
+++ src/pcm/pcm.cpp
@@ -111,11 +111,52 @@ void split(const string& src, const string& left, const string& right) {
     if (channel2.is_open()) {
         channel2.close();
     }
+
+    delete [] sample;
+}
+
+/**
+ * @brief Half the volume of the left channel
+ *
+ * @param src PCM file path
+ * @param dest output PCM file path
+ */
+void volume(const string& src, const string& dest) {
+    std::ifstream in(src, std::ios::in | std::ios::binary);
+    std::ofstream out(dest, std::ios::out | std::ios::binary | std::ios::trunc);
+
+    if (!in.is_open()) {
+        std::cerr << "error: open " << src << " failed!" << std::endl;
+        return;
+    }
+
+    if (!out.is_open()) {
+        std::cerr << "error: open " << dest << " failed!" << std::endl;
+        return;
+    }
+
+    char sample[4];
+
+    while (!in.eof()) {
+        in.read(sample, 4);
+        int16_t* samplenum = reinterpret_cast<int16_t*>(sample);
+        *samplenum = *samplenum / 2;
+        out.write(sample, 4);
+    }
+
+    if (in.is_open()) {
+        in.close();
+    }
+
+    if (out.is_open()) {
+        out.close();
+    }
 }
 
 int main(int argc, char *argv[])
 {
     pcm2wav("../NocturneNo2inEflat_44.1k_s16le.pcm", "pcm2wav.wav", 44100, 16, 2);
     split("../NocturneNo2inEflat_44.1k_s16le.pcm", "channel1.pcm", "channel2.pcm");
+    volume("../NocturneNo2inEflat_44.1k_s16le.pcm", "volume.pcm");
     return 0;
 }
