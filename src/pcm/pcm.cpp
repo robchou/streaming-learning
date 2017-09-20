e1969330c873a3bb67756d03231da7d74e295173
Robin Choud <robin.m.chou@gmail.com>
2017-09-20 14:52:01 +0800

    Add pcm s16le to s8le and u8le

--- src/pcm/pcm.cpp
+++ src/pcm/pcm.cpp
@@ -16,7 +16,7 @@ using std::string;
  * @param bits bits per sample
  * @param channels channels
  */
-void pcm2wav(const string& src, const string& dest, int sample_rate,
+void pcm_to_wav(const string& src, const string& dest, int sample_rate,
         int bits, int channels) {
     RiffChunk riff;
     FormatChunk fmt;
@@ -191,11 +191,86 @@ void speed(const string& src, const string& dest) {
     }
 }
 
+/**
+ * @brief Convert s16le to u8le
+ *
+ * @param src PCM file path
+ * @param dest output PCM file path
+ */
+void pcm16_to_pcm8(const string& src, const string& dest) {
+    std::ifstream in(src, std::ios::in | std::ios::binary);
+    std::ofstream out(dest, std::ios::out | std::ios::binary | std::ios::trunc);
+
+    if (!in.is_open()) {
+        std::cerr << "error: open " << src << " failed!" << std::endl;
+        return;
+    }
+
+    if (!out.is_open()) {
+        std::cerr << "error: open " << dest << " failed!" << std::endl;
+        return;
+    }
+
+    char sample[4];
+
+    while (!in.eof()) {
+        in.read(sample, 4);
+        int16_t* sample16 = reinterpret_cast<int16_t*>(sample);
+        uint8_t sample8[2];
+        sample8[0] = static_cast<uint8_t>(sample16[0] >> 8) + 128;
+        sample8[1] = static_cast<uint8_t>(sample16[1] >> 8) + 128;
+        out.write(reinterpret_cast<char*>(sample8), 2);
+    }
+
+    if (in.is_open()) {
+        in.close();
+    }
+
+    if (out.is_open()) {
+        out.close();
+    }
+}
+
+void pcm16_to_pcm8s(const string& src, const string& dest) {
+    std::ifstream in(src, std::ios::in | std::ios::binary);
+    std::ofstream out(dest, std::ios::out | std::ios::binary | std::ios::trunc);
+
+    if (!in.is_open()) {
+        std::cerr << "error: open " << src << " failed!" << std::endl;
+        return;
+    }
+
+    if (!out.is_open()) {
+        std::cerr << "error: open " << dest << " failed!" << std::endl;
+        return;
+    }
+
+    char sample[4];
+
+    while (!in.eof()) {
+        in.read(sample, 4);
+        int16_t* sample16 = reinterpret_cast<int16_t*>(sample);
+        int8_t sample8[2];
+        sample8[0] = static_cast<int8_t>(sample16[0] >> 8);
+        sample8[1] = static_cast<int8_t>(sample16[1] >> 8);
+        out.write(reinterpret_cast<char*>(sample8), 2);
+    }
+
+    if (in.is_open()) {
+        in.close();
+    }
+
+    if (out.is_open()) {
+        out.close();
+    }
+}
 int main(int argc, char *argv[])
 {
-    pcm2wav("../NocturneNo2inEflat_44.1k_s16le.pcm", "pcm2wav.wav", 44100, 16, 2);
+    pcm_to_wav("../NocturneNo2inEflat_44.1k_s16le.pcm", "pcm2wav.wav", 44100, 16, 2);
     split("../NocturneNo2inEflat_44.1k_s16le.pcm", "channel1.pcm", "channel2.pcm");
     volume("../NocturneNo2inEflat_44.1k_s16le.pcm", "volume.pcm");
     speed("../NocturneNo2inEflat_44.1k_s16le.pcm", "speed.pcm");
+    pcm16_to_pcm8("../NocturneNo2inEflat_44.1k_s16le.pcm", "pcm8.pcm");
+    pcm16_to_pcm8s("../NocturneNo2inEflat_44.1k_s16le.pcm", "pcm8s.pcm");
     return 0;
 }
