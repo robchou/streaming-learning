d981bf4f22cdf66340d306daf310ce40f794b397
Robin Choud <robin.m.chou@gmail.com>
2017-09-20 11:32:40 +0800

    Add split example

--- src/pcm/pcm.cpp
+++ src/pcm/pcm.cpp
@@ -16,7 +16,7 @@ using std::string;
  * @param bits bits per sample
  * @param channels channels
  */
-void pcm2wav(const string& src, const string &dest, int sample_rate,
+void pcm2wav(const string& src, const string& dest, int sample_rate,
         int bits, int channels) {
     RiffChunk riff;
     FormatChunk fmt;
@@ -65,8 +65,57 @@ void pcm2wav(const string& src, const string &dest, int sample_rate,
     }
 }
 
+/**
+ * @brief split PCM stero s16le 44100 into 2 channels
+ *
+ * @param src PCM file path
+ * @param left channel1 output file
+ * @param right channel2 output file
+ */
+void split(const string& src, const string& left, const string& right) {
+    std::ifstream in(src, std::ios::in | std::ios::binary);
+    std::ofstream channel1(left, std::ios::out | std::ios::trunc | std::ios::binary);
+    std::ofstream channel2(right, std::ios::out | std::ios::trunc | std::ios::binary);
+
+    if (!in.is_open()) {
+        std::cerr << "error: open " << src << " failed!" << std::endl;
+        return;
+    }
+
+    if (!channel1.is_open()) {
+        std::cerr << "error: open " << left << " failed!" << std::endl;
+        return;
+    }
+
+    if (!channel2.is_open()) {
+        std::cerr << "error: open " << right << " failed!" << std::endl;
+        return;
+    }
+
+    char* sample = new char[4];
+
+    while (!in.eof()) {
+        in.read(sample, 4);
+        channel1.write(sample, 2);
+        channel2.write(sample + 2, 2);
+    }
+
+    if (in.is_open()) {
+        in.close();
+    }
+
+    if (channel1.is_open()) {
+        channel1.close();
+    }
+
+    if (channel2.is_open()) {
+        channel2.close();
+    }
+}
+
 int main(int argc, char *argv[])
 {
     pcm2wav("../NocturneNo2inEflat_44.1k_s16le.pcm", "pcm2wav.wav", 44100, 16, 2);
+    split("../NocturneNo2inEflat_44.1k_s16le.pcm", "channel1.pcm", "channel2.pcm");
     return 0;
 }
